91141608966a3bb4a8d5109adcdd81b2fac3ffd7
Brynner03 <bventura03@hotmail.com>
2022-06-20 12:14:19 -0400

    Rendering and displaying quotes from dummy data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import QuoteList from '../components/quotes/QuoteList'
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Brynner', text: 'Learning React is fun'},
+    { id: 'q2', author: 'Ventura', text: 'Learning React is okay'},
+]
 
 function AllQuotes() {
   return (
-    <div>AllQuotes</div>
+  <QuoteList quotes={DUMMY_QUOTES} />
   )
 }
 

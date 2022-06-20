742b9c9d16ebdc46dd4d6db1672dc0bc56be8880
Brynner03 <bventura03@hotmail.com>
2022-06-20 11:41:31 -0400

    Added Nested Route to quote details page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { Route, useParams } from 'react-router-dom'
+import { Fragment } from 'react'
+
+import Comments from '../components/comments/Comments'
 
 function QuoteDetail() {
+  const params = useParams()
+
   return (
-    <div>QuoteDetail</div>
+    <Fragment>
+      <div>QuoteDetail</div>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
   )
 }
 

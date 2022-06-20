2f033c9bfc4fc73a631f8bd62f6a44a5ffa7f9e1
Brynner03 <bventura03@hotmail.com>
2022-06-20 14:24:11 -0400

    Added a NotFound Page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,15 +1,26 @@
 import { Route, useParams } from 'react-router-dom'
 import { Fragment } from 'react'
 
+import HighlightedQuote from '../components/quotes/HighlightedQuote'
 import Comments from '../components/comments/Comments'
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Brynner', text: 'Learning React is fun'},
+  { id: 'q2', author: 'Ventura', text: 'Learning React is okay'},
+]
 
 function QuoteDetail() {
   const params = useParams()
 
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+
+  if (!quote) {
+    return <p>No quote found!</p>
+  }
+
   return (
     <Fragment>
-      <div>QuoteDetail</div>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
